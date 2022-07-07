33f038f99d5dc7ba3bb53230c603531147f7802f
ngntuli <mr.ng.ntuli@gmail.com>
2022-07-08 00:28:33 +0200

    use methods for calculating area and perimeter

--- src/main/java/ngntuli/chapter02/CalcAreaAndPerimeter.java
+++ src/main/java/ngntuli/chapter02/CalcAreaAndPerimeter.java
@@ -3,22 +3,31 @@ package main.java.ngntuli.chapter02;
 import java.util.Scanner;
 
 public class CalcAreaAndPerimeter {
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        double length;
-        double height;
-        double area;
-        double perimeter;
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		double length;
+		double height;
+		double area;
+		double perimeter;
 
-        System.out.println("*** Calculate Area and Perimeter");
-        System.out.print("Enter length: ");
-        length = keyboard.nextDouble();
-        System.out.print("Enter height: ");
-        height = keyboard.nextDouble();
-        area = length * height;
-        perimeter = (2 * length) + (2 * height);
+		System.out.println("*** Calculate Area and Perimeter of Rectangle ***");
+		System.out.print("Enter length: ");
+		length = keyboard.nextDouble();
+		System.out.print("Enter height: ");
+		height = keyboard.nextDouble();
 
-        System.out.println("Area = " + area);
-        System.out.println("Perimeter = " + perimeter);
-    }
+		area = calcArea(length, height);
+		perimeter = calcPerimeter(length, height);
+
+		System.out.println("Area = " + area);
+		System.out.println("Perimeter = " + perimeter);
+	}
+
+	static double calcArea(double lengthIn, double heightIn) {
+		return lengthIn * heightIn;
+	}
+
+	static double calcPerimeter(double lengthIn, double heightIn) {
+		return (2 * lengthIn) + (2 * heightIn);
+	}
 }
