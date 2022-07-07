c3c43f82cf1ed01447181b755da220c0870cfe50
ngntuli <mr.ng.ntuli@gmail.com>
2022-07-08 00:22:05 +0200

    Added input validation and menu interface

--- src/main/java/ngntuli/chapter02/CalcAreaAndCircumferenceOfCircle.java
+++ src/main/java/ngntuli/chapter02/CalcAreaAndCircumferenceOfCircle.java
@@ -3,19 +3,62 @@ package main.java.ngntuli.chapter02;
 import java.util.Scanner;
 
 public class CalcAreaAndCircumferenceOfCircle {
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        final double PI = 3.1416;
-        double radius;
-        double area;
-        double circumference;
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		final double PI = 3.1416;
+		double radius = 0.0;
+		double area;
+		double circumference;
+		int reply;
 
-        System.out.println("*** Calculate Area and Circumference of a circle ***");
-        System.out.print("Enter value for the radius of a circle: ");
-        radius = keyboard.nextDouble();
-        area = PI * (radius * radius);
-        circumference = 2 * PI * radius;
-        System.out.println("Area = " + area);
-        System.out.println("Circumference = " + circumference);
-    }
+		System.out.println("*** Calculate Area and Circumference of a Circle ***");
+
+		do {
+			System.out.println();
+			System.out.println("*****************");
+			System.out.println("[1] Set radius");
+			System.out.println("[2] Display radius");
+			System.out.println("[3] Display area");
+			System.out.println("[4] Display perimeter");
+			System.out.println("[5] Quit");
+			System.out.println("*****************");
+			System.out.print("Enter your choice: ");
+			reply = keyboard.nextInt();
+
+			switch (reply) {
+			case 1:
+				System.out.print("Enter value for the radius of a circle: ");
+				radius = keyboard.nextDouble();
+
+				while (radius <= 0.0) {
+					System.out.print("Enter value for the radius of a circle: ");
+					radius = keyboard.nextDouble();
+				}
+				break;
+			case 2:
+				if (radius <= 0.0) {
+					System.out.print("Set radius first");
+				} else {
+					System.out.print("Radius = " + radius);
+				}
+				break;
+			case 3:
+				area = PI * (radius * radius);
+				System.out.println("Area = " + area);
+				break;
+			case 4:
+				circumference = 2 * PI * radius;
+				System.out.println("Circumference = " + circumference);
+				break;
+			case 5:
+				System.out.println("Goodbye!");
+				break;
+			default:
+				System.out.println("Error: wrong input, choice [1-5] ");
+
+			}
+		} while (reply != 5);
+
+	}
+
 }
