b904bd53793ec64415320795bf2a4eb953eedb41
ngntuli <mr.ng.ntuli@gmail.com>
2022-07-08 00:26:00 +0200

    use method

--- src/main/java/ngntuli/chapter02/ConvertsPoundsToKilos.java
+++ src/main/java/ngntuli/chapter02/ConvertsPoundsToKilos.java
@@ -3,15 +3,19 @@ package main.java.ngntuli.chapter02;
 import java.util.Scanner;
 
 public class ConvertsPoundsToKilos {
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        double pounds;
-        double kilosToPounds = 2.2;
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		double pounds;
+		double result;
 
-        System.out.println("*** Converts Pounds to Kilos ***");
-        System.out.print("Enter value in pounds: ");
-        pounds = keyboard.nextDouble();
-        pounds = pounds / kilosToPounds;
-        System.out.println(pounds + "kilos");
-    }
+		System.out.println("*** Converts Pounds to Kilos ***");
+		System.out.print("Enter value in pounds: ");
+		pounds = keyboard.nextDouble();
+		result = convert(pounds);
+		System.out.println(result + " kilos");
+	}
+
+	static double convert(double poundsIn) {
+		return poundsIn / 2.2;
+	}
 }
