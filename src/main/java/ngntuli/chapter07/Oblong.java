de94790b6bb7e7945a565248ec727f7cc04ae2e0
ngntuli <mr.ng.ntuli@gmail.com>
2022-07-20 00:04:55 +0200

    removed comments

--- src/main/java/ngntuli/chapter07/Oblong.java
+++ src/main/java/ngntuli/chapter07/Oblong.java
@@ -1,45 +1,35 @@
 package main.java.ngntuli.chapter07;
 
 public class Oblong {
-	// the attributes
-	private double length;
-	private double height;
+    private double length;
+    private double height;
 
-	// the methods
+    public Oblong(double lengthIn, double heightIn) {
+        length = lengthIn;
+        height = heightIn;
+    }
 
-	// the constructor
-	public Oblong(double lengthIn, double heightIn) {
-		length = lengthIn;
-		height = heightIn;
-	}
+    public double getLength() {
+        return length;
+    }
 
-	// this method allows us to read the length attribute
-	public double getLength() {
-		return length;
-	}
+    public void setLength(double lengthIn) {
+        length = lengthIn;
+    }
 
-	// this method allows us to read the height attribute
-	public double getHeight() {
-		return height;
-	}
+    public double getHeight() {
+        return height;
+    }
 
-	// this method allows us to write to the length attribute
-	public void setLength(double lengthIn) {
-		length = lengthIn;
-	}
+    public void setHeight(double heightIn) {
+        height = heightIn;
+    }
 
-	// this method allows us to write to the height attribute
-	public void setHeight(double heightIn) {
-		height = heightIn;
-	}
+    public double calculateArea() {
+        return length * height;
+    }
 
-	// this method returns the area of the rectangle
-	public double calculateArea() {
-		return length * height;
-	}
-
-	// this method returns the perimeter of the rectangle
-	public double calculatePerimeter() {
-		return 2 * (length + height);
-	}
+    public double calculatePerimeter() {
+        return 2 * (length + height);
+    }
 }
