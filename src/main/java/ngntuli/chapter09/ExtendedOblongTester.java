c12b2a8f2e49ab5913dddac0f79564264d044823
ngntuli <mr.ng.ntuli@gmail.com>
2022-07-27 22:45:40 +0200

    modify to enter a symbol and create object

--- src/main/java/ngntuli/chapter09/ExtendedOblongTester.java
+++ src/main/java/ngntuli/chapter09/ExtendedOblongTester.java
@@ -4,7 +4,12 @@ import main.java.ngntuli.chapter07.EasyScanner;
 
 public class ExtendedOblongTester {
     public static void main(String[] args) {
-        ExtendedOblong extOblong = new ExtendedOblong(10.2, 5.3, '*');
+        char symbol;
+        System.out.print("Enter a symbol: ");
+        symbol = EasyScanner.nextChar();
+
+        ExtendedOblong extOblong = new ExtendedOblong(10.2, 5.3, symbol);
+
         System.out.println(extOblong.draw());
         extOblong.setSymbol('+');
         System.out.println(extOblong.draw());
