6ad6863292d3d0da34d4e0ef7f4716951f12ca2e
ngntuli <mr.ng.ntuli@gmail.com>
2022-07-27 22:44:34 +0200

    import EasyScanner

--- src/main/java/ngntuli/chapter09/ExtendedOblongTester.java
+++ src/main/java/ngntuli/chapter09/ExtendedOblongTester.java
@@ -1,5 +1,7 @@
 package main.java.ngntuli.chapter09;
 
+import main.java.ngntuli.chapter07.EasyScanner;
+
 public class ExtendedOblongTester {
     public static void main(String[] args) {
         ExtendedOblong extOblong = new ExtendedOblong(10.2, 5.3, '*');
