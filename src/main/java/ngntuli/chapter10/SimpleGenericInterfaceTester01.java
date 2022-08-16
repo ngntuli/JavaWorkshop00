6957c8ceeb7316cc32d6121afa23c0019b72bd8f
ngntuli <mr.ng.ntuli@gmail.com>
2022-08-17 00:02:51 +0200

    Simple Generic Interface Tester Version1

---
+++ src/main/java/ngntuli/chapter10/SimpleGenericInterfaceTester01.java
@@ -0,0 +1,15 @@
+package main.java.ngntuli.chapter10;
+
+import java.util.function.Function;
+
+public class SimpleGenericInterfaceTester01 {
+    public static void main(String[] args) {
+        Function<Integer, String> t = new Function<Integer, String>() {
+            @Override
+            public String apply(Integer integer) {
+                return "You entered " + integer;
+            }
+        };
+        System.out.println(t.apply(10));
+    }
+}
