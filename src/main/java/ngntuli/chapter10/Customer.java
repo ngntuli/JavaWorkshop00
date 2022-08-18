7179769b8e7843033c8db32cecffb64e46c3ba15
ngntuli <mr.ng.ntuli@gmail.com>
2022-08-19 00:03:09 +0200

    Customer class

---
+++ src/main/java/ngntuli/chapter10/Customer.java
@@ -0,0 +1,29 @@
+package main.java.ngntuli.chapter10;
+
+public class Customer {
+    private String name;
+    private String id;
+    private double creditLimit;
+
+    public Customer(String name, String id, double creditLimit) {
+        this.name = name;
+        this.id = id;
+        this.creditLimit = creditLimit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+}
