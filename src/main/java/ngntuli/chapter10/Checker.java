bb3c437d3c4fed346bafbb626b70663c7a2a7f3a
ngntuli <mr.ng.ntuli@gmail.com>
2022-08-21 02:28:49 +0200

    Checker class

---
+++ src/main/java/ngntuli/chapter10/Checker.java
@@ -0,0 +1,9 @@
+package main.java.ngntuli.chapter10;
+
+public class Checker {
+    public static void main(String[] args) {
+        CheckableCustomer customer1 = new CheckableCustomer("Givenson", "900523", 10_000.0);
+        CheckableCustomer customer2 = new CheckableCustomer("Charlotte", "170116", 5_000.0);
+
+    }
+}
