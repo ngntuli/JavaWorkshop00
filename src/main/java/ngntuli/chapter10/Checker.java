e6428322543fbc33272b7fa65cb40a7d5f2eaa4a
ngntuli <mr.ng.ntuli@gmail.com>
2022-08-23 00:01:59 +0200

    creates three customer objects

--- src/main/java/ngntuli/chapter10/Checker.java
+++ src/main/java/ngntuli/chapter10/Checker.java
@@ -2,8 +2,25 @@ package main.java.ngntuli.chapter10;
 
 public class Checker {
     public static void main(String[] args) {
-        CheckableCustomer customer1 = new CheckableCustomer("Givenson", "900523", 10_000.0);
-        CheckableCustomer customer2 = new CheckableCustomer("Charlotte", "170116", 5_000.0);
+        CheckableCustomer customer1 = new CheckableCustomer(
+                "Givenson",
+                "X523",
+                10_000.0
+        ); //valid
+        CheckableCustomer customer2 = new CheckableCustomer(
+                "Charlotte",
+                "170116",
+                5_000.0
+        );//invalid
+        CheckableCustomer customer3 = new CheckableCustomer(
+                "Nkosi",
+                "X43x",
+                6_400.0
+        );
+
+        System.out.println("account1 is " + checkValidity(customer1));
+        System.out.println("account2 is " + checkValidity(customer2));
+        System.out.println("account3 is " + checkValidity(customer3));
 
     }
 }
