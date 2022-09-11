e7ba18042bbb19ee2675e15673efe8a10aca94f1
ngntuli <mr.ng.ntuli@gmail.com>
2022-09-11 03:03:03 +0200

    Easy scanner plus class tester

---
+++ src/main/java/ngntuli/chapter14/EasyScannerPlusTester.java
@@ -0,0 +1,14 @@
+package main.java.ngntuli.chapter14;
+
+public class EasyScannerPlusTester {
+    public static void main(String[] args) {
+        int number1;
+        double number2;
+        System.out.print("Enter integer: ");
+        number1 = EasyScannerPlus.nextInt();
+        System.out.println("Integer: " + number1);
+        System.out.print("Enter double: ");
+        number2 = EasyScannerPlus.nextDouble();
+        System.out.println("Double: " + number2);
+    }
+}
