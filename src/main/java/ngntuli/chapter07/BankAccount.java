72b62a13436f60e551b12a346d1d2ec08d9fc5dd
ngntuli <mr.ng.ntuli@gmail.com>
2022-10-06 21:45:32 +0200

    Bank account class

---
+++ src/main/java/ngntuli/chapter07/BankAccount.java
@@ -0,0 +1,38 @@
+package main.java.ngntuli.chapter07;
+
+public class BankAccount {
+    private final String accountNumber;
+    private final String accountName;
+    private double balance;
+
+    public BankAccount(String numberIn, String nameIn) {
+        accountNumber = numberIn;
+        accountName = nameIn;
+        balance = 0;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amountIn) {
+        balance = balance + amountIn;
+    }
+
+    public boolean withdraw(double amountIn) {
+        if (amountIn > balance) {
+            return false;
+        } else {
+            balance = balance - amountIn;
+            return true;
+        }
+    }
+}
