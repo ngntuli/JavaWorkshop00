92d3fdd1666333dc6f35f82f4e5bae2604a840b5
ngntuli <mr.ng.ntuli@gmail.com>
2022-10-24 16:26:34 +0200

    push me button app

--- src/main/java/ngntuli/chapter10/PushMe.java
+++ src/main/java/ngntuli/chapter10/PushMe.java
@@ -30,13 +30,24 @@ public class PushMe extends Application {
         pushMeButton.setText("Type something in the box then push me");
         pushMeButton.setOnAction(e -> pushMeLabel.setText("You entered: " + pushMeTextField.getText()));
 
+        Button buttonDisplayUpperCase = new Button();
+        buttonDisplayUpperCase.setText("Display UPPER CASE LETTER");
+        buttonDisplayUpperCase.setOnAction(
+                e -> pushMeLabel.setText("You entered: " + pushMeTextField.getText().toUpperCase()));
+
+        Button buttonDisplayLength = new Button();
+        buttonDisplayLength.setText("Display Length");
+        buttonDisplayLength.setOnAction(
+                e -> pushMeLabel.setText("Length: " + pushMeTextField.getText().length()));
+
         VBox root = new VBox();
         root.setSpacing(10);
         root.setAlignment(Pos.CENTER);
 
-        root.getChildren().addAll(pushMeTextField, pushMeButton, pushMeLabel);
+        root.getChildren()
+                .addAll(pushMeTextField, pushMeButton, buttonDisplayUpperCase, buttonDisplayLength, pushMeLabel);
 
-        Scene scene = new Scene(root, 350, 150);
+        Scene scene = new Scene(root, 350, 250);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Push Me");
